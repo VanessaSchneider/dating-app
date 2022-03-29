0b1fea64faa18f49bdba2d8fe8bfb720b3eca30d
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-03-29 17:57:24 -0400

    validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   has_one :match
 
+  
+
   def match_check
     liked_person = self.liked_person_id 
     matched_like = Like.where(user_id: liked_person, liked_person_id: self.user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :likes 
   has_many :matches, through: :likes
   has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
